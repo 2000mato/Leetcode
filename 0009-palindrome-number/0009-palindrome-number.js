864426d17cbcb858b5dfe611a62aff70b47b0f5b
Mato <billytomasello1@gmail.com>
2023-07-27 21:06:48 -0400

    Time: 144 ms (99.06%), Space: 52.1 MB (7.31%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+    let string = x.toString()
+    let reverseString = string.split('').reverse('').join('')
+
+    if (string == reverseString){
+        return true
+    }
+    else {
+        return false
+    }
+};
